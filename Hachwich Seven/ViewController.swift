4c5f7fdf292d8ab4c8c4a4f5d75b8a40015af048
Alexis Decker <deckera@hawaii.edu>
2023-10-12 13:54:48 -1000

    If you are reading this, I restarted my hackwich seven as I needed to redo it after the deletion of the initial instructions. new project will be called "New Hackwich Seven"

--- Hachwich Seven/ViewController.swift
+++ Hachwich Seven/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        displayLabel.text = (" ")
+        
     }
-
-
+    
+    @IBAction func setLabelButtonPressed(_ sender: Any)
+    {
+        displayLabel.text = (sender as AnyObject).text
+    }
 }
 
